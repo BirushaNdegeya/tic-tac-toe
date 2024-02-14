1f9918fe619ea4e0321f615d42c5f904f9d2e37b
Birusha Ndegeya <142086785+BirushaNdegeya@users.noreply.github.com>
2024-02-14 09:12:48 +0200

    update
    update

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from 'react-dom/client';
+import Square from "./components/Square";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+
+const App = () => {
+   return (
+      <React.Fragment>
+         <Square />
+      </React.Fragment>
+   );
+};
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
