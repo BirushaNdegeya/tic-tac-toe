6d93f4910bb17f9e7aa4fcc47345bab5f5a7ca28
Birusha Ndegeya <142086785+BirushaNdegeya@users.noreply.github.com>
2024-02-14 11:53:07 +0200

    update
    update

--- src/components/Square.jsx
+++ src/components/Square.jsx
@@ -1,7 +1,16 @@
 import React from "react";
 
-const Square = () => {
-   return <button className="square">1</button>
+const Square = ( { value } ) => {
+
+   const handleClick = () => {
+      console.log(`Clicked the button number ${value}`);
+   }
+
+   return (
+      <button 
+         className="square"
+         onClick={handleClick}>{value}</button>
+   );
 };
 
 export default Square;
