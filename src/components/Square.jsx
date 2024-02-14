0d3af2b4cadb5e521417829026eff523ca627b5d
Birusha Ndegeya <142086785+BirushaNdegeya@users.noreply.github.com>
2024-02-14 09:20:19 +0200

    update
    update

--- src/components/Square.jsx
+++ src/components/Square.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Square = () => {
-   return <button className="square">X</button>
+   return <button className="square"><h1>X</h1></button>
 };
 
 export default Square;
